39867f39266dc702d85339c17d33b15bd514a9ea
SergioPaula <encontre.sergio@gmail.com>
2024-09-01 14:20:52 -0300

    swiperjs

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
-let swiper = new Swiper('.swiper', {
-    direction: 'horizontal'
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
     loop: true,
   
-
-    // pagination: {
-    //   el: '.swiper-pagination',
-    // },
-
-    // navigation: {
-    //   nextEl: '.swiper-button-next',
-    //   prevEl: '.swiper-button-prev',
-    // },
-});
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });

85ae58af5531732c4c72fb4493d9ed7b27e67393
SergioPaula <encontre.sergio@gmail.com>
2025-02-14 08:49:07 -0300

    test carrossel
    teste com ajuda do deepseek

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
-var swiper = new Swiper(".cardapio-produtos", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  freeMode: true,
-
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1.5, // Mostra 1 card e meio por vez
+  spaceBetween: 10, // Espaço entre os cards
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next', // Botão de avançar
+      prevEl: '.swiper-button-prev', // Botão de voltar
   },
+  breakpoints: {
+      640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+      },
+      768: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+      },
+      1024: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+      },
+  }
 });

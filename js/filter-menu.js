e86f9908562f14b4a89eaac54968f460cdb8a4c3
SergioPaula <encontre.sergio@gmail.com>
2025-04-26 12:52:14 -0300

    segundo filtro - TAG
    criação do filtro por TAG

--- js/filter-menu.js
+++ js/filter-menu.js
@@ -3,7 +3,7 @@ const itemsFilter = document.querySelectorAll('.item-filter');
 itemsFilter.forEach(item => {
     item.addEventListener('click', () => {
         const textFilter = item.closest('.filtro-produto').querySelector('.text-filtro');
-        
+
         // Se o texto clicado já está ativo, apenas remove a classe (esconde)
         if (textFilter.classList.contains('active')) {
             textFilter.classList.remove('active');
@@ -18,6 +18,9 @@ itemsFilter.forEach(item => {
         textFilter.classList.add('active');
     });
 });
+
+
+//ORIENTAÇÃO DO CURSO KIWIFY
 
 $(document).ready(function () {
     cardapio.eventos.init();
@@ -43,5 +46,62 @@ cardapio.metodos = {
 }
 
 cardapio.templates = {
+
+    <div class="swiper-slide" >
+        <article class="product-card" data-category="sem-cobertura">
+            <figure class="card-image">
+                <img src="./img/PRODUTOS/01-chocolate/01.png" alt="Bolo de Chocolate" />
+                <div class="tags">
+                    <span class="tag sale">Promoção</span>
+                </div>
+                <div class="title-overlay">
+                    <h2>Bolo de Chocolate <br>com Brigadeiro e Granulado</h2>
+                </div>
+            </figure>
+
+            <div class="card-content">
+                <div class="rating">
+                    <svg>
+                        <use href="./img/ICONS/icones-gerais.svg#star-full"></use>
+                    </svg>
+                    <svg>
+                        <use href="./img/ICONS/icones-gerais.svg#star-full"></use>
+                    </svg>
+                    <svg>
+                        <use href="./img/ICONS/icones-gerais.svg#star-full"></use>
+                    </svg>
+                    <svg>
+                        <use href="./img/ICONS/icones-gerais.svg#star-full"></use>
+                    </svg>
+                    <svg class="star-line">
+                        <use href="./img/ICONS/icones-gerais.svg#star-line"></use>
+                    </svg>
+
+                    <p class="avaliacoes">236 Avaliações</p>
+                </div>
+
+
+                <p class="description">Delicioso bolo de chocolate coberto com brigadeiro cremoso e
+                    chocolate belga ralado. Uma explosão de sabor para os amantes de chocolate.</p>
+
+                <div class="price-container">
+                    <span class="old-price">R$ 89,90</span>
+                    <span class="current-price">R$ 129,90</span>
+                    <p class="medida-unidade">/un</p>
+                </div>
+
+                <div class="purchase-controls">
+                    <div class="quantity">
+                        <button class="qty-btn minus" data-product="1">-</button>
+                        <span class="qty-display">1</span>
+                        <button class="qty-btn plus" data-product="1">+</button>
+                    </div>
+                    <button class="add-to-cart" data-product="1">
+                        Adicionar à Sacola
+                    </button>
+                </div>
+            </div>
+        </article>
+    </div>
 
 }
